22fcb7bb3fb089a2922e4c936d8dd103be150cba
danielestefani1 <36449183+danielestefani1@users.noreply.github.com>
2022-03-04 15:24:16 +0100

    Add files via upload

---
+++ watchOS-3-heartrate-master copy 2/VimoHeartRate/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  Daniele Stefani
+//  15/04/2019
+//
+//
+//
+//
+
+import UIKit
+import AVKit
+import AVFoundation
+
+class ViewController: UIViewController {
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        playVideo()
+    }
+    
+    private func playVideo() {
+        guard let path = Bundle.main.path(forResource: "shoulder-press-500-4", ofType:"mp4") else {
+            debugPrint("video.m4v not found")
+            return
+        }
+        let player = AVPlayer(url: URL(fileURLWithPath: path))
+        let playerController = AVPlayerViewController()
+        playerController.player = player
+        present(playerController, animated: true) {
+            player.play()
+        }
+    }
+}
+
